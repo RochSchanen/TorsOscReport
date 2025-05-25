7640b432c36245f3221b399ead7fa8a29ea7f2a7
Roch Schanen <r.schanen@lancaster.ac.uk>
2025-05-25 09:58:45 +0100

    add gitignore, update intro

--- ComputeData.py
+++ ComputeData.py
@@ -1,25 +1,23 @@
 
-dico = {}
+def exportcommands(file, dico):
+    OB, CB = r"{", r"}"
+    _FH = open(file, 'w')
+    for k in dico.keys():
+        value, units = dico[k]
+        nc = f"\\newcommand\\{k}{OB}\\si{OB}\\num{OB}{value}{CB}{units}{CB}{CB}"
+        _FH.write(f"{nc}\n")
+    _FH.close()
+
+
+################################################################################
 
 from scipy.constants import epsilon_0
-dico['epsilonzero'] = f"{epsilon_0:.3E}", f"\\Farad\\per\\meter"    
-
-V = 200 # [V]
-dico['drivepotential'] = f"{V}", "\\volt"
+V = 200
 
 
+################################################################################
 
-
-
-
-
-
-
-############################################################################
-OB, CB = r"{", r"}"
-_FH = open("data.tex", 'w')
-for k in dico.keys():
-    value, units = dico[k]
-    nc = f"\\newcommand\\{k}{OB}\\si{OB}\\num{OB}{value}{CB}{units}{CB}{CB}"
-    _FH.write(f"{nc}\n")
-_FH.close()
+exportcommands("data.tex", {
+    'epsilonzero'       : (f"{epsilon_0:.3E}",      f"\\farad\\per\\meter"),    
+    'drivepotential'    : (f"{V}",                  f"\\volt")
+    })
